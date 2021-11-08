18e454f952d2dac8543f91bd29198237e7df203b
iceboxi <iceboxidev@gmail.com>
2021-11-08 10:16:03 +0800

    refactor: remove unused library remove IITool

--- Hahow-iOS-Recruit/Model/Model.swift
+++ Hahow-iOS-Recruit/Model/Model.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import IITool
 
 // MARK: - Hahow
 struct Hahow: Model {
@@ -27,3 +26,30 @@ struct Course: Model {
     let category: String
 }
 
+public protocol Model: Codable, Equatable {
+    init?(data: Data)
+    func encode() -> Data?
+}
+
+extension Model {
+    public init?(data: Data) {
+        do {
+            self = try JSONDecoder().decode(Self.self, from: data)
+        } catch {
+            print("Decode Error: \(error)")
+            return nil
+        }
+    }
+    
+    public func encode() -> Data? {
+        do {
+            return try JSONEncoder().encode(self)
+        } catch {
+            return nil
+        }
+    }
+    
+    public static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.encode() == rhs.encode()
+    }
+}
